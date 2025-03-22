6febccb1271280abefb0f97c30d98148d97144cb
auto <auto@example.com>
2025-03-23 01:19:56 +0800

    完整项目重构

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        typescript: true
+      }
+    })
+  ],
+  build: {
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+})
